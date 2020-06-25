8cea62f9dd61fc6c3ae69c8d79e6dc5824f121d7
GIsidori <45291585+GIsidori@users.noreply.github.com>
2020-06-25 19:02:32 +0200

    Commit iniziale

---
+++ xMax.Module/BusinessObjects/Enum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xMax.Module.BusinessObjects
+{
+    public enum TipoDDT
+    {
+        Acquisto,
+        Vendita
+    }
+}
