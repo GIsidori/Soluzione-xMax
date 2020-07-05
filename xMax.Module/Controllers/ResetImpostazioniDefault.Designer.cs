e8d393f158c4fcfd9a61032052d8356cb8dd370d
Giorgio <giorgio160586@gmail.com>
2020-07-05 12:35:48 +0200

    aggiunto controller ResetImpostazioniDefault

---
+++ xMax.Module/Controllers/ResetImpostazioniDefault.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace xMax.Module.Controllers
+{
+    partial class ResetImpostazioniDefault
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.simpleAction1 = new DevExpress.ExpressApp.Actions.SimpleAction(this.components);
+            // 
+            // simpleAction1
+            // 
+            this.simpleAction1.Caption = "Reset impostazioni default";
+            this.simpleAction1.ConfirmationMessage = "Si è sicuri di voler resettare tutte le impostazioni?";
+            this.simpleAction1.Id = "e5ff1cd3-a70f-4774-9a9a-04b69cb0bfa7";
+            this.simpleAction1.ImageName = "Actions_DeleteCircled";
+            this.simpleAction1.TargetObjectType = typeof(xMax.Module.BusinessObjects.Database.Impostazioni);
+            this.simpleAction1.TargetViewNesting = DevExpress.ExpressApp.Nesting.Root;
+            this.simpleAction1.TargetViewType = DevExpress.ExpressApp.ViewType.ListView;
+            this.simpleAction1.ToolTip = null;
+            this.simpleAction1.TypeOfView = typeof(DevExpress.ExpressApp.ListView);
+            this.simpleAction1.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.simpleAction1_Execute);
+            // 
+            // ResetImpostazioniDefault
+            // 
+            this.Actions.Add(this.simpleAction1);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction simpleAction1;
+    }
+}
